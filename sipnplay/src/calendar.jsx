5aa3ee224b755125cc02c810d016b96b8600c9db
Joan Elizabeth <ninjanerd101@gmail.com>
2024-07-11 16:40:50 -0500

    React & Calendar
    Created project in React with Vite
    Started making the calendar

---
+++ sipnplay/src/calendar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Calendar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Calendar</h1>
+            </div>
+        );
+    }
+}
